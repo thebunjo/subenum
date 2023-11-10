82dc61b1a691fa82a21849b5cc6b5ecdce7bea42
BunjoThe <bunjothe@gmail.com>
2023-11-10 14:57:04 +0300

    subenum

---
+++ subenum.rb
@@ -0,0 +1,39 @@
+require 'net/http'
+
+def banner
+  banner_text = <<-'BANNER'
+____ _  _ ___  ____ _  _ _  _ _  _ 
+[__  |  | |__] |___ |\ | |  | |\/|     Written by Bunjo.
+___] |__| |__] |___ | \| |__| |  |     Github: https://github.com/thebunjo
+                               
+  BANNER
+end
+
+puts banner.cyan
+
+print "Enter target (ex: google.com): ".magenta
+$domain = gets.chomp.to_s
+
+$file_path = "subdomains.txt"
+$file = File.open($file_path)
+$content = $file.read
+$subdomains = $content.split("\n")
+
+def find(subdomain)
+  url = "http://#{subdomain}.#{$domain}"
+  begin
+    response = Net::HTTP.get_response(URI(url))
+  rescue Exception
+    # Ignored
+  else
+    puts "[+] Discovered subdomain: #{url}".green
+  end
+end
+
+threads = []
+
+$subdomains.each do |subdomain|
+  threads << Thread.new { find(subdomain) }
+end
+
+threads.each(&:join)
